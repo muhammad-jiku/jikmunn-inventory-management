c401c3bae63589574b612116b61a7127b3ae1efe
muhammad-jiku <muhammadjiku364@gmail.com>
2025-06-24 22:01:58 +0600

    server init

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
